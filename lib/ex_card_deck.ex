cfd8eecdca5bfaedb623873812f9edf1b0733b14
Jonathan C. Otsuka <djgoku@gmail.com>
2017-09-06 23:52:22 -0500

    Add unicode integer and list of values for the card to returned tuple for ExCardDeck.get_card()

--- lib/ex_card_deck.ex
+++ lib/ex_card_deck.ex
@@ -20,7 +20,7 @@ defmodule ExCardDeck do
   ## Examples
 
       iex> ExCardDeck.get_card
-      {:heart, "2"}
+      {:heart, "2", 127154, [2]}
 
       # When all cards have been dealt.
       iex> ExCardDeck.get_card
@@ -73,11 +73,11 @@ defmodule ExCardDeck do
   end
 
   def deck() do
-    card_ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    card_suits = [:clubs, :diamonds, :hearts, :spades]
-    Enum.map(card_ranks, fn(rank) ->
-      Enum.map(card_suits, fn(suit) ->
-        {suit, rank}
+    card_ranks = [{"A", 0, [1,11]}, {"2", 1, [2]}, {"3", 2, [3]}, {"4", 3, [4]}, {"5", 4, [5]}, {"6", 5, [6]}, {"7", 6, [7]}, {"8", 7, [8]}, {"9", 8, [9]}, {"10", 9, [10]}, {"J", 10, [10]}, {"Q", 11, [10]}, {"K", 12, [10]}]
+    card_suits = [{:clubs, 127185}, {:diamonds, 127169}, {:hearts, 127153}, {:spades, 127137}]
+    Enum.map(card_ranks, fn({rank, rank_adder, values}) ->
+      Enum.map(card_suits, fn({suit, unicode_integer}) ->
+        {suit, rank, unicode_integer + rank_adder, values}
       end)
     end)
   end
--- test/ex_card_deck_test.exs
+++ test/ex_card_deck_test.exs
@@ -2,21 +2,21 @@ defmodule ExCardDeckTest do
   use ExUnit.Case
 
   test "get_card" do
-    deck = [{:hearts, "A"}, {:spades, "2"}]
-    {:reply, card, new_state} = ExCardDeck.handle_call(:get_card, :from, %{decks: deck})
-    assert card == {:hearts, "A"}
+    state = %{decks: [{:hearts, "A", 1, [1,11]}, {:spades, "2", 2, [2]}]}
+    {:reply, card, new_state} = ExCardDeck.handle_call(:get_card, :from, state)
+    assert card == {:hearts, "A", 1, [1,11]}
 
     {:reply, card, new_state} = ExCardDeck.handle_call(:get_card, :from, new_state)
-    assert card == {:spades, "2"}
+    assert card == {:spades, "2", 2, [2]}
 
     {:reply, card, _new_state} = ExCardDeck.handle_call(:get_card, :from, new_state)
     assert is_nil(card) === true
   end
 
   test "discard" do
-    state = %{decks: [{:hearts, "A"}, {:spades, "2"}]}
+    state = %{decks: [{:hearts, "A", 1, [1,11]}, {:spades, "2", 2, [2]}]}
     {:noreply, new_state} = ExCardDeck.handle_cast(:discard, state)
-    assert new_state[:decks] == [{:spades, "2"}]
+    assert new_state[:decks] == [{:spades, "2", 2, [2]}]
 
     {:noreply, new_state} = ExCardDeck.handle_cast(:discard, new_state)
     assert new_state[:decks] == []
